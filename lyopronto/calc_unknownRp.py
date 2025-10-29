Fix unit conversion: output fraction (0-1) not percentage (0-100)
BREAKING CHANGE: Column 6 (fraction_dried) now outputs as fraction (0-1)
instead of percentage (0-100) for consistency with documentation.

Modified functions:
- opt_Pch.dry(): Divide percent_dried by 100.0 before output
- opt_Tsh.dry(): Divide percent_dried by 100.0 before output
- opt_Pch_Tsh.dry(): Divide percent_dried by 100.0 before output
- calc_unknownRp.dry(): Divide percent_dried by 100.0 before output

This aligns with the documented output format where column 6 should be
a fraction between 0 and 1, not a percentage between 0 and 100.

lyopronto/calc_unknownRp.py
@@ -80,11 +80,11 @@ def dry(vial,product,ht,Pchamber,Tshelf,time,Tbot_exp):
         dL = (dmdt*constant.kg_To_g)*dt[iStep]/(1-product['cSolid']*constant.rho_solution/constant.rho_solute)/(vial['Ap']*constant.rho_ice)*(1-product['cSolid']*(constant.rho_solution-constant.rho_ice)/constant.rho_solute) # cm
 
         # Update record as functions of the cycle time
-        if (iStep==0):
-            output_saved =np.array([[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried]])
+        if iStep == 0:
+            output_saved =np.array([[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried/100.0]])
             product_res =np.array([[t, float(Lck), float(Rp)]])
         else:
-            output_saved = np.append(output_saved, [[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried]],axis=0)
+            output_saved = np.append(output_saved, [[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried/100.0]],axis=0)
             product_res = np.append(product_res, [[t, float(Lck), float(Rp)]],axis=0)
     
         # Advance counters
@@ -118,7 +118,7 @@ def dry(vial,product,ht,Pchamber,Tshelf,time,Tbot_exp):
             else:
                 Pch = max(Pchamber['setpt'][j-1] - Pchamber['ramp_rate']*constant.hr_To_min*(t-Pchamber['t_setpt'][j-1]),Pchamber['setpt'][j])
           
-    output_saved = np.append(output_saved, [[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried]],axis=0)
+    output_saved = np.append(output_saved, [[t, float(Tsub), Tbot_exp[iStep], Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried/100.0]],axis=0)
     product_res = np.append(product_res, [[t, float(Lck), float(Rp)]],axis=0)
 
     ######################################################

lyopronto/opt_Pch.py
@@ -86,10 +86,10 @@ def dry(vial,product,ht,Pchamber,Tshelf,dt,eq_cap,nVial):
         dL = (dmdt*constant.kg_To_g)*dt/(1-product['cSolid']*constant.rho_solution/constant.rho_solute)/(vial['Ap']*constant.rho_ice)*(1-product['cSolid']*(constant.rho_solution-constant.rho_ice)/constant.rho_solute) # cm
 
         # Update record as functions of the cycle time
-        if (iStep==0):
-            output_saved =np.array([[t, float(Tsub), float(Tbot), Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried]])
+        if iStep == 0:
+            output_saved =np.array([[t, float(Tsub), float(Tbot), Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried/100.0]])
         else:
-            output_saved = np.append(output_saved, [[t, float(Tsub), float(Tbot), Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried]],axis=0)
+            output_saved = np.append(output_saved, [[t, float(Tsub), float(Tbot), Tsh, Pch*constant.Torr_to_mTorr, dmdt/(vial['Ap']*constant.cm_To_m**2), percent_dried/100.0]],axis=0)
     
         # Advance counters
         Lck_prev = Lck # Previous cake length in cm